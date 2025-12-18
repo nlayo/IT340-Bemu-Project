173438a610cd0b9a46bb64c20d7f10a9f81dc45f
nlayo <mv466@njit.edu>
2025-12-18 11:30:48 -0500

    Began creating the search page, modified search bar placeholder in indexhtml.Created new products.js that is a mini database with all of the static products on the home page and will be used for the seargch page.

---
+++ product.js
@@ -0,0 +1,30 @@
+window.PRODUCTS = [
+  {
+    id: "p1",
+    name: "Cozy Hoodie",
+    price: 39.99,
+    image: "images/cozyhoodie.webp",
+    tags: ["clothing", "hoodie", "warm"]
+  },
+  {
+    id: "p2",
+    name: "Non-stick Cookware Set",
+    price: 89.99,
+    image: "images/cookwareset.jpg",
+    tags: ["kitchen", "cookware"]
+  }
+  {
+    id: "p3",
+    name: "Watches",
+    price: 120.99,
+    image: "images/gizmowatch.avif",
+    tags: ["Watch"]
+  },
+  {
+    id: "p4",
+    name: "Pear Wireless Headphones",
+    price: 220.99,
+    image: "images/wirelessheadphones.jpg",
+    tags: ["Headphones", "wireless"]
+  }
+];
